f101050786606390667d6ece34435421128ecd85
Nukala Somesh <nukalas@infinite.com>
2020-11-10 04:32:06 +0530

    main header and main right section common with test alert

---
+++ js/common-sections.js
@@ -0,0 +1,9 @@
+$(document).ready(
+    function(){
+      // alert("ojkkjdfkd");
+      alert('hi git');
+       $("#mainHeader").load("main-header.html");
+
+       $("#mainRightSecion").load("main-right-section.html");
+    }
+)
