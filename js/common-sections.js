5f284069bd79d3e585ba8edb5522719cf16db5c6
Nukala Somesh <nukalas@infinite.com>
2020-11-10 07:20:51 +0530

    playstore and appstore icons added in footer

--- js/common-sections.js
+++ js/common-sections.js
@@ -2,8 +2,10 @@ $(document).ready(
     function(){
       // alert("ok ok");
        $("#mainHeader").load("main-header.html");
-
        $("#mainRightSection").load("main-right-section.html");
+       $("#mainFooter").load("main-footer.html");
+
+      // alert(location.pathname.search('my-claims.html'));
     }
     
 )
