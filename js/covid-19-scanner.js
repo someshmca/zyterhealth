5999d8019c04f12c4e0f87e9a85cf279b1f9fe59
Nukala Somesh <nukalas@infinite.com>
2020-12-04 10:02:06 +0530

    covid screen checkbox validations and footer changes

--- js/covid-19-scanner.js
+++ js/covid-19-scanner.js
@@ -20,3 +20,48 @@ function gotoSection(sectionName){
     $("#covidScreen7").hide();
     $('#'+sectionName).show();
 }
+
+function check_covidform1(screen){
+		if($('#covidform1 input[type=checkbox]:checked').length == 0)
+		{
+			$("#covidform1 #checkboxError1").show();
+        }		
+        else{
+            $("#covidform1 #checkboxError1").hide();
+            gotoSection(screen);
+        }
+}
+
+function check_covidform2(screen){
+    if($('#covidform2 input[type=checkbox]:checked').length == 0)
+    {
+        $("#covidform2 #checkboxError2").show();
+    }		
+    else{
+        $("#covidform2 #checkboxError2").hide();
+        gotoSection(screen);
+    }
+}
+
+function check_covidform3(screen){
+    if($('#covidform3 input[type=radio]:checked').length == 0)
+    {
+        $("#covidform3 #checkboxError3").show();
+    }		
+    else{
+        $("#covidform3 #checkboxError3").hide();
+        gotoSection(screen);
+    }
+}
+
+function check_covidform4(screen){
+    if($('#covidform4 input[type=radio]:checked').length == 0)
+    {
+        $("#covidform4 #checkboxError4").show();
+    }		
+    else{
+        $("#covidform4 #checkboxError4").hide();
+        gotoSection(screen);
+    }
+}
+
